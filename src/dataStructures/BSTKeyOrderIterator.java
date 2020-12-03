0b61124fd236be4425c98749b89e6ae8c7e1290b
Afonso Batista <amf.batista@campus.fct.unl.pt>
2020-12-03 16:22:37 +0000

    Iterator commented

--- src/dataStructures/BSTKeyOrderIterator.java
+++ src/dataStructures/BSTKeyOrderIterator.java
@@ -6,6 +6,9 @@ import dataStructures.BinarySearchTree.BSTNode;
  * Binary Search Tree Key Order Iterator implementation
  * @author Afonso Batista 57796  
  * @author Joao Jorge 57994  
+ * 
+ * @param <K> Generic type Key.
+ * @param <V> Generic type Value. 
  */  
 
 public class BSTKeyOrderIterator<K,V> implements Iterator<Entry<K,V>> {
--- src/dataStructures/IteratorHashTable.java
+++ src/dataStructures/IteratorHashTable.java
@@ -1,10 +1,15 @@
 package dataStructures;
 
+/**
+ * Iterator hash table implementation.
+ * @author Afonso Batista 57796  
+ * @author Joao Jorge 57994  
+ * 
+ * @param <K> Generic type Key.
+ * @param <V> Generic type Value. 
+ */
 public class IteratorHashTable<K,V> implements Iterator<Entry<K,V>> {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -3757572178582017026L;
 	
 	private Entry<K, V> next;
@@ -37,12 +42,16 @@ public class IteratorHashTable<K,V> implements Iterator<Entry<K,V>> {
 		return returnNext;
 	}
 	
+	/**
+	 * Move on to the next element in the table.
+	 */
 	private void updateNext() {
+		Iterator<Entry<K,V>> it;
 		while(pos<table.length-1) {
 			pos++;
-			Iterator<Entry<K,V>> it = table[pos].iterator();
+			it = table[pos].iterator();
 			if(it.hasNext()) {    												
-				next= it.next();
+				next = it.next();
 				iterator = it;
 				return;
 			}
